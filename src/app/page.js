02ce9c9d1fcd83d8a0d2fd5705bb7c43645e061f
Ahmad Faisal <achmad_fsl@yahoo.com>
2024-05-05 22:38:34 +0700

    sample landing page

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Screen1 from './components/Screen1';
+import Screen2 from './components/Screen2';
+
+export default function Home() {
+  return (
+    <main className='h-full'>
+      <Screen1 />
+      <Screen2 />
+    </main>
+  );
+}
